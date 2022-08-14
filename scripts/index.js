b22cf437f6348680959995ba59d2016e601a9fcc
AndreyYaro <yarovikov93@yandex.ru>
2022-08-14 09:32:43 +0300

    sunday morning

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,31 @@
 const popup = document.querySelector('.popup')
 const editButton = document.querySelector('.button_type_edit')
 const closeButton = document.querySelector('.button_type_close')
-const saveButton = document.querySelector('.button_type_save')
-
-
-const togglePopup = function () {
-    popup.classList.toggle('popup__opened');
-}
-
-let formElement = document.querySelector('.popup__input') 
+let formElement = document.querySelector('.button_tupe_save') 
 let nameInput = document.querySelector('.popup__input_name')
 let jobInput = document.querySelector('.popup__input_info') 
 let profileName = document.querySelector('.profile__name')
 let profileProfession = document.querySelector('.profile__profession')
 
-nameInput.value = profileName.textContent
-jobInput.value = profileProfession.textContent
+
+const addPopup = function () {
+    popup.classList.add('popup__opened');
+    nameInput.value = profileName.textContent
+    jobInput.value = profileProfession.textContent
+}
+
+const removePopup = function () {
+    popup.classList.remove('popup__opened');
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-
     profileName.textContent = nameInput.value 
     profileProfession.textContent = jobInput.value
+    removePopup
 }
-editButton.addEventListener('click', togglePopup)
-closeButton.addEventListener('click', togglePopup)
-saveButton.addEventListener('click', togglePopup) 
-saveButton.addEventListener('click', formSubmitHandler)
+
+editButton.addEventListener('click', addPopup)
+closeButton.addEventListener('click', removePopup)
 
 formElement.addEventListener('submit', formSubmitHandler);
