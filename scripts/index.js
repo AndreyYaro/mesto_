7e1578c181b8f5ed7c96a8b7e2e368a53913ad02
AndreyYaro <yarovikov93@yandex.ru>
2022-08-14 12:10:37 +0300

    first iteration

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@
 const popup = document.querySelector('.popup')
 const editButton = document.querySelector('.button_type_edit')
 const closeButton = document.querySelector('.button_type_close')
-let formElement = document.querySelector('.button_tupe_save') 
+
+let popupForm = document.querySelector('.popup__content') 
 let nameInput = document.querySelector('.popup__input_name')
 let jobInput = document.querySelector('.popup__input_info') 
 let profileName = document.querySelector('.profile__name')
@@ -18,15 +19,15 @@ const addPopup = function () {
 const removePopup = function () {
     popup.classList.remove('popup__opened');
 }
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    profileName.textContent = nameInput.value 
-    profileProfession.textContent = jobInput.value
-    removePopup
-}
-
 editButton.addEventListener('click', addPopup)
 closeButton.addEventListener('click', removePopup)
 
-formElement.addEventListener('submit', formSubmitHandler);
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value; 
+    profileProfession.textContent = jobInput.value;
+    console.log('hihi');
+    removePopup();
+}
+
+popupForm.addEventListener('submit', formSubmitHandler);
