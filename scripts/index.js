431a90712393762f8c1a37327b2a426fc46986a2
AndreyYaro <yarovikov93@yandex.ru>
2022-08-23 21:58:26 +0300

    Первый коммит в новой ветке

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,54 @@
 // Объявляем переменные 
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup_type_edit')
 const editButton = document.querySelector('.button_type_edit')
 const closeButton = document.querySelector('.button_type_close')
+const addButton = document.querySelector('.popup_type_edit')
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const listElement = document.querySelector('.elements')
+
+//  Функция для массива
+initialCards.forEach(name =>{
+    let html = `
+        <article class="element">
+            <img src=
+                alt="${name}"
+                class="element__image">
+            <div class="element__name-like">
+                <h2 class="element__name">${name}</h2>
+                <button type="button" class="button element__like" aria-label="нравится"></button>
+            </div>
+        </article>
+          `
+    listElement.insertAdjacentHTML('afterbegin', html)
+})
+
+    
 
 // Объявляем переменные формы Поп-апа
 let popupForm = document.querySelector('.popup__content') 
@@ -35,4 +82,5 @@ function formSubmitHandler (evt) {
 // Добавляем слушатели действий для кнопок 
 editButton.addEventListener('click', addPopup)
 closeButton.addEventListener('click', removePopup)
+
 popupForm.addEventListener('submit', formSubmitHandler);
