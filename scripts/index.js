a359ffd172ae33b654700a850f4d0d5f6ea9e189
AndreyYaro <yarovikov93@yandex.ru>
2022-08-29 12:24:55 +0300

    первая итерация прооверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,10 @@
 // Объявляем переменные 
 const editButton = document.querySelector('.button_type_edit')
 const closeEditButton = document.querySelector('.button_type_close-edit')
-
 const addButton = document.querySelector('.button_type_plus')
 const closeAddButton = document.querySelector('.button_type_close-add')
-
 const popupEdit = document.querySelector('.popup_type_edit')
 const popupAdd = document.querySelector('.popup_type_add')
-
 const likeButton = document.querySelector('.element__like')
 
 // Объявляем переменные формы Поп-апа(1)
@@ -27,7 +24,7 @@ let linkInput = document.querySelector('.popup__info_type_link')
 const popupCard = document.querySelector('.popup-card')
 const popupImages = document.querySelector('.popup-card__img')
 const popupText = document.querySelector('.popup-card__text')
-const popupCardClose = document.querySelector('.button_type_close-card')
+const closePopupImg = document.querySelector('.button_type_close-card')
 
 let templateElement = document.querySelector('.template')
 
@@ -57,6 +54,22 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
+// Функция лайка
+  function likeToggle(e) {
+    const itemElement = e.target.closest('.element__like')
+    itemElement.classList.toggle('element__like_active')
+  }
+//  Функция урны
+  function moveToTrash(e) {
+    const itemElement = e.target.closest('.element')
+    itemElement.remove()
+  }
+// Открытие Попапа картинки
+  function openImgPopup (item){
+    popupCard.classList.add('popup_opened')
+    popupImages.src = item.link
+    popupText.textContent = item.name
+}
 
 //  Функция для добавления карточки
 function addNewCard(item){
@@ -65,33 +78,33 @@ function addNewCard(item){
     newItemElement.querySelector('.element__name').textContent = item.name
     newItemElement.querySelector('.element__image').setAttribute('src', item.link)
 
-    newItemElement
-        .querySelector('.element__like')
-        .addEventListener('click', (e) => {
-            const itemElement = e.target.closest('.element__like')
-            itemElement.classList.toggle('element__like_active')
-        })
-
-    newItemElement
-        .querySelector('.element__trash')
-        .addEventListener('click', (e) => {
-            const itemElement = e.target.closest('.element')
-            itemElement.remove()
-        })
-
-    newItemElement
-        .querySelector('.element__image')
-        .addEventListener('click', ()=> {
-            popupCard.classList.add('popup_opened')
-            popupImages.src = item.link
-            popupText.textContent = item.name
-        })
-
+    newItemElement.querySelector('.element__like').addEventListener('click', likeToggle)
+    newItemElement.querySelector('.element__trash').addEventListener('click', moveToTrash)
+    newItemElement.querySelector('.element__image').addEventListener('click', ()=> openImgPopup (item))
+        
     listElement.prepend(newItemElement)
 }
-
+    
 initialCards.forEach(addNewCard)
-
+    
+// Функция добавления карточки
+function formAddHandler(evt){
+    evt.preventDefault();    
+    let text = placeInput.value
+    let linkAdd = linkInput.value
+    placeInput.value = ''
+    linkInput.value = ''
+    addNewCard({name: text,link: linkAdd})
+    removePopup(popupAdd)  
+}
+          
+// Функция отправки заполненной формы редактирования имени  
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value; 
+    profileProfession.textContent = jobInput.value;
+    removePopup(popupEdit);
+}
 
 //  Функция открытия Поп-апа редактирования
 function addPopupEdit() {
@@ -99,58 +112,26 @@ function addPopupEdit() {
     nameInput.value = profileName.textContent
     jobInput.value = profileProfession.textContent
 }
-// Функция закрытия Поп-апа редактироования
-const removePopupEdit = function () {
-    popupEdit.classList.remove('popup_opened');
-}
+
 //  Функция открытия Поп-апа создания
 function addPopupAdd() {
     popupAdd.classList.add('popup_opened')
 }
-// Функция закрытия Поп-апа создания
-const removePopupAdd = function () {
-    popupAdd.classList.remove('popup_opened');
-}
 
-// функция закрытия попапа картинки
-const removePopupImage = function () {
-    popupCard.classList.remove('popup_opened');
-}
-
-// Функция отправки заполненной формы редактирования имени
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    profileName.textContent = nameInput.value; 
-    profileProfession.textContent = jobInput.value;
-    removePopupEdit();
-}
-// функция добавления карточки
-function formAddHandler(evt){
-    evt.preventDefault();    
-    let text = placeInput.value
-    let linkAdd = linkInput.value
-    placeInput.value = ''
-    linkInput.value = ''
-    addNewCard(
-        {
-            name: text,
-            link: linkAdd
-        }
-    )
-    removePopupAdd()  
+ // Функция закрытия Попапа картинки
+function removePopup (item) {
+    item.classList.remove('popup_opened');
 }
 
 // Добавляем слушатели действий для кнопок 
-editButton.addEventListener('click', addPopupEdit)
-closeEditButton.addEventListener('click', removePopupEdit)
+closeEditButton.addEventListener('click', () => removePopup(popupEdit))
+closeAddButton.addEventListener('click', () => removePopup(popupAdd))
+closePopupImg.addEventListener('click', () => removePopup(popupCard))
 
 addButton.addEventListener('click', addPopupAdd)
-closeAddButton.addEventListener('click', removePopupAdd)
-
-popupCardClose.addEventListener('click', removePopupImage)
+editButton.addEventListener('click', addPopupEdit)
 
 
 popupEditForm.addEventListener('submit',formSubmitHandler);
-
 popupAddFoam.addEventListener('submit', formAddHandler)
 
